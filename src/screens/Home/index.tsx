1aaa8b62e71b9861f06a93eb1fdcef630d34c574
tiagoolivv <suubmundo@gmail.com>
2021-10-09 16:16:19 -0300

    feat: header home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
+import { StatusBar } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+import Logo from '../../assets/logo.svg';
 
 import {
   Container,
-  Title,
+  Header,
+  HeaderContent,
+  TotalCars
 } from './styles';
 
 export const Home: React.FC = () => {
   return (
     <Container>
-      <Title>Home</Title>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor='transparent'
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo height={RFValue(12)} width={RFValue(108)}/>
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
+      </Header>
     </Container>
   );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,14 +1,27 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
   background-color: ${({ theme }) => theme.colors.background.primary};
 `;
 
-export const Title = styled.Text`
-  font-size: 30px;
-  color: ${({ theme }) => theme.colors.shape.dark};
-  font-family: ${({ theme }) => theme.fonts.archivo.semibold};
+export const Header = styled.View`
+  width: 100%;
+  height: 113px;
+  background-color: ${({ theme }) => theme.colors.header};
+  justify-content: flex-end;
+`;
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 24px 32px;
+`;
+
+export const TotalCars = styled.Text`
+  color: ${({ theme }) => theme.colors.text.primary};
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.inter.regular};
 `;
